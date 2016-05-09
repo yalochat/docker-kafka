5a9e877cdf8aa2558be3e0d228a745ca66662d79
jolivares <juan.o.lazcano@gmail.com>
2016-05-09 09:16:45 +0200

    set ocsp port

--- scripts/start-kafka.sh
+++ scripts/start-kafka.sh
@@ -99,8 +99,8 @@ if [ ! -z "$SSL_CLIENT_AUTH" ]; then
 fi
 
 # OCSP
-if [ ! -z "$SSL_OCSP" ]; then
-    echo -e "ocsp.enable=true\nocsp.responderURL=http://localhost:8000" > $KAFKA_HOME/config/security.properties
+if [ ! -z "$SSL_OCSP_PORT" ]; then
+    echo -e "ocsp.enable=true\nocsp.responderURL=http://localhost:$SSL_OCSP_PORT" > $KAFKA_HOME/config/security.properties
     export KAFKA_OPTS="-Djava.security.debug=all -Dcom.sun.security.enableCRLDP=true -Dcom.sun.net.ssl.checkRevocation=true -Djava.security.properties=$KAFKA_HOME/config/security.properties $KAFKA_OPTS"
 fi
 
--- scripts/start-ocsp.sh
+++ scripts/start-ocsp.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect
 
-if {[info exists ::env(SSL_OCSP)] && [info exists ::env(SSL_OCSP_DIR)]} {
-	spawn openssl ocsp -port 8000 -index $::env(SSL_OCSP_DIR)/index.txt -CA $::env(SSL_OCSP_DIR)/ca-cert \
+if {[info exists ::env(SSL_OCSP_PORT)] && [info exists ::env(SSL_OCSP_DIR)]} {
+	spawn openssl ocsp -port $::env(SSL_OCSP_PORT) -index $::env(SSL_OCSP_DIR)/index.txt -CA $::env(SSL_OCSP_DIR)/ca-cert \
 	-rsigner $::env(SSL_OCSP_DIR)/ca-cert -rkey $::env(SSL_OCSP_DIR)/ca-key -text
 
 	expect "Enter pass phrase for"
